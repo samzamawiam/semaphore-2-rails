f7892bec2f4adb7779b0f90a1c29cbe719987298
Adam Simon <adamjamessimon@gmail.com>
2021-02-24 23:04:45 -0500

    rails g rolemodel:testing:all

---
+++ app/javascript/components/test_component.js
@@ -0,0 +1,10 @@
+const React = require('react')
+
+class TestComponent extends React.Component {
+
+  render() {
+    return <div>THIS COMPONENT CAN RENDER</div>
+  }
+
+}
+module.exports = TestComponent
